e0ea25b8645f73d2736c916c31f3e3c6ca187534
vmbreath <vmbreath@gmail.com>
2020-05-30 22:05:17 +0300

    ver 1.3

---
+++ docs/precache-manifest.9c30632109e17e4143a0d40a66ac8e1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3308fde634388038d119925d9e286858",
+    "url": "/workingproject/index.html"
+  },
+  {
+    "revision": "0492206bf87969cef741",
+    "url": "/workingproject/static/css/2.46517760.chunk.css"
+  },
+  {
+    "revision": "c5ca7324e413b66c8f0d",
+    "url": "/workingproject/static/css/main.476dd163.chunk.css"
+  },
+  {
+    "revision": "0492206bf87969cef741",
+    "url": "/workingproject/static/js/2.fa4e2e5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/workingproject/static/js/2.fa4e2e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ca7324e413b66c8f0d",
+    "url": "/workingproject/static/js/main.17d858c6.chunk.js"
+  },
+  {
+    "revision": "8f62c93aa333b541e07e",
+    "url": "/workingproject/static/js/runtime-main.b580419c.js"
+  }
+]);
